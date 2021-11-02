a4d9e5bf12b4d9859ff7cdb060c835b2a556b49a
Ignatovw99 <lyuboslavw@gmail.com>
2021-11-02 10:22:37 +0200

    employee and office models created

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import env_config
 
+
 app = Flask(__name__)
 app.config.from_object(env_config[app.config["ENV"]])
 db = SQLAlchemy(app)
+
 
 from app import views
--- app/models.py
+++ app/models.py
@@ -14,3 +14,26 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User(first_name={self.first_name}, last_name={self.last_name})"
+
+
+class Employee(db.Model):
+    __tablename__ = "employees"
+
+    id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    office_id = db.Column(db.Integer, db.ForeignKey("offices.id"))
+    user = db.relationship(User, foreign_keys=id, lazy="joined")
+
+    def __repr__(self):
+        return f"Employee(first_name={self.user.first_name}, last_name={self.user.last_name}, office_id={self.office_id})"
+
+
+class Office(db.Model):
+    __tablename__ = "offices"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    address = db.Column(db.String(200), nullable=False)
+    employees = db.relationship(Employee, backref="office", lazy="select")
+
+    def __repr__(self):
+        return f"Office(name={self.name}, address={self.address})"
